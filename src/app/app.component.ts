d3dad0b900d81fff1d909464c89b058d7a61e0bf
Nitin Negi <nitinnegi0810@gmail.com>
2019-09-27 17:50:08 +0530

    prototype done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,83 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'stage-time-graph';
+
+  timerId: any;
+  arms = [
+    {
+    name: 'Arm1',
+    id: 1,
+    lights: [],
+  },
+  {
+    name: 'Arm2',
+    id: 1,
+    lights: [],
+  },
+  {
+    name: 'Arm3',
+    id: 1,
+    lights: [],
+  }
+];
+
+  timeStamps: Array<Date> = [];
+
+  constructor() {
+
+  }
+
+  setTimeStamps() {
+
+    this.timeStamps=[];
+    const now = new Date();
+
+    for (let i = 0; i <= 7; i++) {
+
+      const time = new Date(0, 0, 0, now.getHours(), now.getMinutes() - 2 * i, now.getSeconds());
+      this.timeStamps.unshift(time);
+    }
+  }
+
+  startTimer() {
+    this.timerId = setInterval(() => {
+
+      this.setTimeStamps();
+
+      this.arms.forEach((arm: any) => {
+        const lastColor = arm.lights[arm.lights.length - 1];
+        lastColor.width += 10;
+      });
+
+    }, 1000);
+  }
+
+
+  onAddColor() {
+
+    clearInterval(this.timerId);
+
+    this.arms.forEach((arm: any) => {
+      arm.lights.push({ color: this.giveColor(), width: 0 });
+    });
+
+    this.startTimer();
+  }
+
+
+  giveColor() {
+    const r = Math.random();
+    if (r <= 0.4) {
+      return 'red';
+    } else if (r > 0.4 && r <= 0.6) {
+      return 'green';
+    } else {
+      return 'yellow';
+    }
+  }
+
+
 }
